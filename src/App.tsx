c187f8e4f4a5ce0ee649d2147a78c09544a60346
Andriikryl <andreyk557@gmail.com>
2024-03-17 16:01:34 +0200

    git init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Hero from "./components/hero/Hero";
+import Palettes from "./components/palettes/Palettes";
+import "./styles/global.css";
+function App() {
+  return (
+    <main>
+      <Hero />
+      <Palettes />
+    </main>
+  );
+}
+
+export default App;
